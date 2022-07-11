58c8357d4808eb962216eca3be061ca4ff21b70b
Brent Denny <brent.denny@ddls.com.au>
2022-07-11 10:17:07 +1000

    ps to add the demo ou

--- UpSellWithPS/UpSellWithPS.ps1
+++ UpSellWithPS/UpSellWithPS.ps1
@@ -2,8 +2,10 @@
 Param()
 
 Clear-Host
-Write-Host "Creating 200 users"
+Write-Host "Creating users"
 $NewUsers = Import-Csv -Path .\NewUsers.csv
+$DemoOuExists = Test-Path 'AD:\OU=Demo,DC=Adatum,DC=com'
+if ($DemoOuExists -eq $false) {New-ADOrganizationalUnit -Path 'DC=Adatum,DC=com' -Name 'Demo' -ErrorAction 'Stop'}
 foreach ($NewUser in $NewUsers) {
   $NewUser |
    Select-Object -Property *,@{n='AccountPassword';e={$_.ClearPassword | ConvertTo-SecureString -AsPlainText -Force}} -ExcludeProperty ClearPassword |
