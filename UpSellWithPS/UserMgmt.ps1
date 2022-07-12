a880fce0fecee04ddb316002288123e519b272a7
BrentD09 <brent.denny@ddls.com.au>
2022-07-11 17:26:59 -0700

    added status messages

--- UpSellWithPS/UserMgmt.ps1
+++ UpSellWithPS/UserMgmt.ps1
@@ -1,10 +1,10 @@
-﻿<# 
+<# 
 .NAME
     ChangeAccount
 #>
 
 $AllUsers = Get-ADUser -filter * 
-$AllNames = $AllUsers.Name
+$AllNames = $AllUsers | Sort-Object -Property Name
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
@@ -16,14 +16,22 @@ $Form.TopMost                    = $false
 
 $dabtn                           = New-Object system.Windows.Forms.Button
 $dabtn.text                      = "Disable Account"
-$dabtn.width                     = 115
+$dabtn.width                     = 150
 $dabtn.height                    = 30
 $dabtn.location                  = New-Object System.Drawing.Point(606,240)
 $dabtn.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
+$statuslbl                       = New-Object System.Windows.Forms.Label
+$statuslbl.text                  = ""
+$statuslbl.width                 = 300
+$statuslbl.height                = 30
+$statuslbl.location              = New-Object System.Drawing.Point(36,100)
+$statuslbl.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
+$statuslbl.ForeColor             = "Red"
+
 $eabtn                           = New-Object system.Windows.Forms.Button
 $eabtn.text                      = "Enable Account"
-$eabtn.width                     = 115
+$eabtn.width                     = 150
 $eabtn.height                    = 30
 $eabtn.location                  = New-Object System.Drawing.Point(606,290)
 $eabtn.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
@@ -34,13 +42,36 @@ $sucbox.height                   = 78
 $sucbox.location                 = New-Object System.Drawing.Point(36,50)
 $sucbox.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',14)
 $sucbox.Items.AddRange($AllNames)
+$sucbox.DisplayMember = 'Name'
 
-$Form.controls.AddRange(@($dabtn,$eabtn,$sucbox))
+$Form.controls.AddRange(@($statuslbl,$dabtn,$eabtn,$sucbox))
+
+$sucbox.Add_SelectedValueChanged({
+  $ADUser = Get-ADUser -Identity $sucbox.SelectedItem
+  $statuslbl.ForeColor = "Black"
+  if ($ADUser.Enabled -eq $true) {$statuslbl.text = $ADUser.Name + ' is currently enabled'}
+  else {$statuslbl.text = $ADUser.Name + ' is currently disabled'}
+}) 
+
 
 $dabtn.Add_Click({ 
-    if ($sucbox.SelectedItem) {}
+  if ($sucbox.SelectedItem) {
+    $statuslbl.ForeColor = "Red"
+    Disable-ADAccount -Identity $sucbox.SelectedItem
+    $ADUser = Get-ADUser -Identity $sucbox.SelectedItem
+    if ($ADUser.Enabled -eq $false) {$statuslbl.text = $ADUser.Name + ' has been disabled'}
+
+  }
  })
-$eabtn.Add_Click({ EnableAccount })
+$eabtn.Add_Click({ 
+  if ($sucbox.SelectedItem) {
+    $statuslbl.ForeColor = "Blue"
+    Enable-ADAccount -Identity $sucbox.SelectedItem
+    $ADUser = Get-ADUser -Identity $sucbox.SelectedItem
+    if ($ADUser.Enabled -eq $true) {$statuslbl.text = $ADUser.Name + ' has been enabled'}
+  }
+   
+})
 $sucbox.Add_Click({ GetUsers })
 
 #region Logic 
